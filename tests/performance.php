adf2ed4cf6e77745c8b7a799d60458e0bdd9b7fb
Hiraku Nakano <hiraku@tojiru.net>
2013-04-06 14:56:53 +0900

    update XML_Builder 1.0.9 -> 1.0.10

--- tests/performance.php
+++ tests/performance.php
@@ -1,43 +1,31 @@
 <?php
-class PerformanceTest extends PHPUnit_Framework_TestCase
-{
-    function testTimePlist() {
-        $b = xml_builder(array('class'=>'XML_Builder_Plist'));
+require dirname(__FILE__) . '/../vendor/autoload.php';
 
-        $start = microtime(true);
+$builders = array(
+    'plist' => xml_builder(array('class'=>'plist')),
+    'xmlwriter' => xml_builder(array(
+        'class'=>'xmlwriter',
+        'formatOutput'=>false,
+    )),
+    'dom' => xml_builder(array(
+        'class' => 'dom',
+        'formatOutput' => false,
+    )),
+    'json' => xml_builder(array('class' => 'json')),
+    'serialize' => xml_builder(array('class' => 'serialize')),
+);
 
-        $b->root
-            ->_do(function($b){
-                for ($i=0; $i<1000; $i++) {
-                    $b->xmlElem('hoge')->xmlText($i)->xmlEnd();
-                }
-            })
-        ->_;
+foreach ($builders as $name => $b) {
+    $start = microtime(true);
 
-        $arr = $b->_render();
-        file_put_contents('foo.plist', $arr);
+    $b->root
+        ->_do(function($b){
+            for ($i=0; $i<1000; $i++) {
+                $b->xmlElem('hoge')->xmlText($i)->xmlEnd();
+            }
+        })
+    ->_;
 
-
-        echo 'plist: ', microtime(true) - $start, PHP_EOL;
-    }
-
-    function testTimeXMLWriter() {
-        $b = xml_builder(array('class'=>'xmlwriter', 'writeto'=>'foo.xml', 'formatOutput'=>false));
-
-        $start = microtime(true);
-
-        $b->root
-            ->_do(function($b){
-                for ($i=0; $i<1000; $i++) {
-                    $b->xmlElem('hoge')->xmlText($i)->xmlEnd();
-                }
-            })
-        ->_;
-
-        $arr = $b->_render();
-        //file_put_contents('foo.plist', $arr);
-
-
-        echo 'xmlwriter: ', microtime(true) - $start, PHP_EOL;
-    }
+    $arr = $b->_render();
+    echo "$name: ", microtime(true) - $start, PHP_EOL;
 }
